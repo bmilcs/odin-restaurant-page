334d7198c95157545181b95da64bfad5297021e9
Bryan Miller <bmilcs@yahoo.com>
2022-10-07 15:59:50 -0400

    Configure dev environment: package.json & webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,59 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // remove unused files
+  },
+  mode: "development",
+  devtool: "inline-source-map", // debugging
+  devServer: {
+    static: "./dist",
+  },
+  optimization: {
+    runtimeChunk: "single", // dev-server
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page: Odin Project #10",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // css
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i, // sass
+        use: [
+          // 3. Creates `style` nodes from JS strings
+          "style-loader",
+          // 2. Translates CSS into CommonJS
+          "css-loader",
+          // 1. Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                outputStyle: "compressed",
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // images
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // fonts
+        type: "asset/resource",
+      },
+    ],
   },
 };
