d981cba27df0ea3a6c435d0d442a591b0676360f
bmilcs <bmilcs@yahoo.com>
2022-10-09 13:39:08 -0400

    Add basic error checking for createElement()

--- src/utils/create-element.js
+++ src/utils/create-element.js
@@ -1,11 +1,18 @@
-function createElement(type, classes, content, id) {
+function createElement(type, classes, textContentOrAlt, id) {
+  // minimum requirements: element type
+  if (!type) return;
+
   const element = document.createElement(type);
 
-  type === "img" ? (element.alt = content) : (element.textContent = content);
+  // add classes, separated by spaces
+  if (classes) classes.split(" ").forEach((cls) => element.classList.add(cls));
 
-  classes.split(" ").forEach((cls) => element.classList.add(cls));
+  type === "img"
+    ? (element.alt = textContentOrAlt)
+    : (element.textContent = textContentOrAlt);
 
   if (id) element.id = id;
+
   return element;
 }
 
