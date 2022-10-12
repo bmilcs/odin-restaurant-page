e1b4be8fa7cb114cbd262981982bf98a9d86d604
Bryan Miller <bmilcs@yahoo.com>
2022-10-12 14:52:50 -0400

    Fix comments in footer.js

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -4,6 +4,7 @@ import "../scss/footer.scss";
 import githubSVG from "../img/github.svg";
 
 const prepareFooter = () => {
+  // footer container contains an anchor tag with 2 children: p & svg
   const footerContainer = createElement("div", "footer-container");
 
   const link = createElement(
@@ -14,13 +15,14 @@ const prepareFooter = () => {
     "https://github.com/bmilcs/odin-restaurant-page"
   );
 
-  // SVG is loaded as raw text, allowing us to inject it
-  // directly into the innerHTML of the link tag
-
   const p = createElement("p", "footer-p", "bmilcs");
-
   link.appendChild(p);
+
+  // github svg is loaded as raw text, allowing us to inject it
+  // directly into the innerHTML of <a> (link)
+
   link.innerHTML += githubSVG.trim();
+
   footerContainer.appendChild(link);
 
   return footerContainer;
