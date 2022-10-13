db28276c5a9be20e83bef2ed96619385c4f646b6
Bryan Miller <bmilcs@yahoo.com>
2022-10-13 17:02:06 -0400

    Add & style contact section

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -4,9 +4,24 @@ import "../scss/contact.scss";
 
 const prepareContact = () => {
   const contactContainer = createElement("div", "contact-container");
-  const h1 = createElement("h1", "contact-h1", "Contact Us");
 
-  contactContainer.appendChild(h1);
+  const contactElements = [
+    createElement("h1", "contact-h1", "Contact Us"),
+    createElement("h3", "contact-h3", "We want to hear from you."),
+    createElement(
+      "p",
+      "contact-p",
+      "How's our food? What did you think of our staff? Was your experience nothing short of excellent?"
+    ),
+    createElement("h2", "contact-h2", "How are we doing?"),
+    createElement("p", "contact-address", "Burger World"),
+    createElement("p", "contact-address", "86 Somewhere Rd."),
+    createElement("p", "contact-address", "Los Angeles, CA"),
+    createElement("p", "contact-address", "90210"),
+    createElement("p", "contact-address", "(999) 819-1919"),
+  ];
+
+  contactElements.forEach((element) => contactContainer.appendChild(element));
 
   return contactContainer;
 };
